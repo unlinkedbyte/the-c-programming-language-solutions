9db25c791b70b9883aab30d5d2bdb43f7abc9e3d
YGMsec <ygmsec@gmail.com>
2026-05-20 13:27:50 +0200

    feat: solve chapter 1 exercise 1.1 and update gitignore

---
+++ chapter-01/main.c
@@ -0,0 +1,8 @@
+// Exercise 1-1: Run the "hello, world" program on your system. Experiment with leaving out partss of the program, to see what error messages you get
+
+#include <stdio.h>
+
+int main() {
+  printf("hello, world\n");
+  return 0;
+}
